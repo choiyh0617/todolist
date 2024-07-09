ef023784bb8c4156d69bdf61ecb70843044932fd
choiyh0617 <6l0mrc00057@gmail.com>
2024-07-09 19:49:40 +0900

    todolist

--- todolist.js
+++ todolist.js
@@ -1,2 +1,10 @@
+const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const input = document.querySelector('input');
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    console.log(input.value);
+    const li = document.createElement('li');
+    li.innerText = input.value;
+    ul.appendChild(li);
+})
