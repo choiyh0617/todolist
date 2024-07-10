a71d949dd80ac28d70017a69ba805193ab20fbbf
choiyh0617 <6l0mrc00057@gmail.com>
2024-07-10 17:42:07 +0900

    todolist

--- todolist.js
+++ todolist.js
@@ -1,17 +1,8 @@
 const form = document.querySelector('form');
-const input = document.querySelector('input');
-const title = document.querySelector('div')
+const input = document.querySelector('#input');
 const ch = document.querySelector('.ch');
 const detail = document.querySelector('.detail');
-const del = document.querySelector('.del');
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log(input.value);
-    const li = document.createElement('li');
-    li.innerText = input.value;
-    ul.appendChild(li);
-});
+const ex = document.querySelector('#ex');
 
 function checked() {
     if (ch.checked) {
@@ -21,4 +12,40 @@ function checked() {
     }
 }
 
-ch.addEventListener('change', checked);
+document.addEventListener('DOMContentLoaded', () => {
+    ch.addEventListener('click', (event) => {
+        const item = document.createElement('div') // div
+
+        const checkBox = document.createElement('input') // 완료
+        checkBox.setAttribute('type','checkbox')
+
+
+        const text = document.createElement('span') // 내용
+        text.textContent = input.value
+
+        const removeButton = document.createElement('button') // 삭제
+        removeButton.textContent = '삭제'
+
+
+        removeButton.addEventListener('click', (event) => {
+            event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode)
+})
+
+        checkBox.addEventListener('change', (event) => {
+            if(checkBox.checked){
+                text.style.textDecorationLine = "line-through"
+            }else{
+                text.style.textDecorationLine = "none"
+            }
+        })
+                
+        item.appendChild(checkBox)
+        item.appendChild(text)
+        item.appendChild(removeButton)
+
+        ex.appendChild(item)
+        input.value = ''
+        })
+
+
+})
