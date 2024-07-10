dedeefb1b83fed012d91891e219f9019c20b8704
choiyh0617 <6l0mrc00057@gmail.com>
2024-07-10 16:19:55 +0900

    todolist

--- todolist.js
+++ todolist.js
@@ -1,10 +1,24 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
-const detail = document.querySelector('.detail')
-form.addEventListener('submit',(event)=>{
+const title = document.querySelector('div')
+const ch = document.querySelector('.ch');
+const detail = document.querySelector('.detail');
+const del = document.querySelector('.del');
+
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(input.value);
     const li = document.createElement('li');
     li.innerText = input.value;
     ul.appendChild(li);
-})
+});
+
+function checked() {
+    if (ch.checked) {
+        detail.style.textDecoration = 'line-through';
+    } else {
+        detail.style.textDecoration = 'none';
+    }
+}
+
+ch.addEventListener('change', checked);
